f27626979a2bbb11e3d290c3b126aa1ed8cf9f8c
Junedale <gayetajunedale0@gmail.com>
2022-12-12 14:19:52 +0800

    Allow model to use timestamp

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -24,7 +24,7 @@ class UserModel extends Model
     ];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
